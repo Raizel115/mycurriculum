50d2dc0793cbb2fe5adbbcd4f82598d267b2a8c2
GUSTAVO HENRIQUE REINERT <gustavo.reinert1@alunos.sc.senac.br>
2021-06-24 17:13:46 -0300

    Atividade-git-curriculo

---
+++ assets/js/app.js
@@ -0,0 +1,78 @@
+
+
+var sobre = function(){
+    console.log('Funciona!');
+    let divsobre = document.getElementById('sobre');
+    let divhabilidades = document.getElementById('habilidades');
+    divsobre.style.display = 'block';
+    divhabilidades.style.display = 'none';
+}
+
+var habilidades = function(){
+    console.log('Funciona!');
+    let divsobre = document.getElementById('sobre');
+    let divhabilidades = document.getElementById('habilidades');
+    divsobre.style.display = 'none';
+    divhabilidades.style.display = 'block';
+}
+
+var formacao = function(){
+    console.log('Funciona!');
+    let divformacao = document.getElementById('sobre');
+    let divhabilidades = document.getElementById('hobbies');
+    divformacao.style.display = 'block';
+    divhabilidades.style.display = 'none';
+}
+
+var hobbies = function(){
+    console.log('Funciona!');
+    let divformacao = document.getElementById('sobre');
+    let divhabilidades = document.getElementById('hobbies');
+    divformacao.style.display = 'block';
+    divhabilidades.style.display = 'none';
+}
+
+var books = function(){
+    console.log('Funciona!');
+    let divformacao = document.getElementById('sobre');
+    let divhabilidades = document.getElementById('hobbies');
+    divformacao.style.display = 'block';
+    divhabilidades.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
